da75d0f13319b29ab056b6fa085e96ed34358686
Ronan <dineen1@gmail.com>
2023-11-15 10:03:30 +0100

    add Footer About and 404 components with basic Routing

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -15,12 +15,16 @@ function Navbar({title}) {
                 </Link>
             </div>
 
-        <div className="flex-1 px-2 mx-2">
-            <div className="flex justify-end">
-                <Link></Link>
+            <div className="flex-1 px-2 mx-2">
+                <div className="flex justify-end">
+                    <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+                        Home
+                    </Link>
+                    <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+                        About
+                    </Link>
+                </div>
             </div>
-        </div>
-
         </div>
     </nav>
   )
---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. 
+         Completed by
+        <strong>
+          <a href='https://dev-collectiv.com'> Ronan Dineen</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </>
+  )
+}
+
+export default About
