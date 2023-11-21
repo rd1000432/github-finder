2d1c0f36b608fc968042f13bff68154e9f09a86c
Ronan <dineen1@gmail.com>
2023-11-21 11:27:23 +0100

    add alert functions and reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -17,23 +17,32 @@ export const GithubProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    // Get initial users (testing purposes)
-    const fetchUsers = async () => {
+    // Get search results
+    const searchUsers = async (text) => {
         setLoading()
 
-        const response = await fetch(`${GITHUB_URL}/users`, {
+        const params = new URLSearchParams({
+            q: text
+        }) 
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
 
         dispatch({
             type: 'GET_USERS', 
-            payload: data,
+            payload: items,
         })
     }
+
+
+    //Clear users from state
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS'})
+
 
     //Set loading
 
@@ -42,7 +51,8 @@ export const GithubProvider = ({ children }) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
     }}>
         {children}
     </GithubContext.Provider>
