6e8b86e2100e0dd125cfe547ad46b3e164383112
Ronan <dineen1@gmail.com>
2023-11-21 12:25:55 +0100

    add single User component and get data

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -12,6 +12,7 @@ export const GithubProvider = ({ children }) => {
 
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }  
 
@@ -40,6 +41,32 @@ export const GithubProvider = ({ children }) => {
     }
 
 
+        // Get single user
+        const getUser = async (login) => {
+            setLoading()
+    
+            const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`
+                }
+            })
+
+            if(response.status === 404) {
+                window.location = '/notfound'
+            } else {
+              const data = await response.json()
+        
+              dispatch({
+                type: 'GET_USER', 
+                payload: data,
+            })
+
+            }
+    
+            
+        }
+
+
     //Clear users from state
     const clearUsers = () => dispatch({ type: 'CLEAR_USERS'})
 
@@ -51,8 +78,10 @@ export const GithubProvider = ({ children }) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
         clearUsers,
+        getUser,
     }}>
         {children}
     </GithubContext.Provider>
